44cb4957c1379dc1137d4aee1e7abaa1aa79226d
Alex Nemirovsky <anemirovsky@digibank.co.il>
2022-08-14 22:47:39 +0300

    add argocd app of apps

--- terrafform-aws/lab-aws-cilium-cni-chain-main/modules/kubernetes_resources/argocd_application/argocd_application.tf
+++ terrafform-aws/lab-aws-cilium-cni-chain-main/modules/kubernetes_resources/argocd_application/argocd_application.tf
@@ -18,6 +18,7 @@ resource "helm_release" "argocd" {
   repository  = "https://argoproj.github.io/argo-helm"
   chart       = "argo-cd"
   
+  values    = [file("../../../values.yaml")]
   set {
     name  = "server.service.type"
     value = "NodePort"
